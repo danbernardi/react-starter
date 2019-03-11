e0dd5f40c4d4de9e490c2d2998d66fd0f5521b83
danbernardi <danbernardi1@gmail.com>
2019-03-10 22:13:34 -0700

    Adds imgur api request

--- src/containers/AppContainer/index.js
+++ src/containers/AppContainer/index.js
@@ -3,26 +3,58 @@ import { connect } from 'react-redux';
 import { func, oneOfType, node, array } from 'prop-types';
 import { setActiveBreakpoint } from 'redux/actions';
 import { initReduxBreakpoints } from 'utils/responsiveHelpers';
+import { get } from 'lodash';
 import 'styles/core.scss';
+import './AppContainer.scss';
 
 class AppContainer extends React.Component {
-  componentDidMount () {
+  constructor(props) {
+    super(props);
+
+    this.state = { dogs: null };
+  }
+
+  async componentDidMount() {
     const { dispatch } = this.props;
+    const clientId = process.env.REACT_APP_CLIENT_ID;
+    console.log(clientId);
+    const apiUrl = 'https://api.imgur.com/3/gallery/top/all/';
 
+		const fetchConfig = {
+      method: 'GET',
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Authorization: `Client-ID ${clientId}`
+      }),
+      mode: 'cors'
+    }
+    
 
+    fetch(apiUrl, fetchConfig)
+      .then(res => res.json()).then(res => {
+        this.setState({ data: res.data })
+      })
+      .catch(err => console.log(err));
+    
     initReduxBreakpoints.call(
       this, window, (breakpointName, breakpointSize, mediaQueryState) =>
         dispatch(setActiveBreakpoint(breakpointName, breakpointSize, mediaQueryState))
     );
-  }
+	}
 
   render () {
     const { children } = this.props;
+    const { data } = this.state;
+    if (!data) return false;
 
     return (
       <div className="appcontainer">
-        <div className="pagecontent">
-          { children }
+        <div className="pagecontent row">
+          { data.filter(d => d.images_count > 0).map((d, index) => (
+            <div key={ index } className="img-item">
+              <img src={ get(d, 'images[0].link') } alt={ d.description } />
+            </div>
+          )) }
         </div>
       </div>
     );
